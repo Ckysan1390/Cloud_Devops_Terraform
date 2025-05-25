9806fe6081f94a7219ea880342005fcc51ecae86
Santiago Rivas <ckysan.139@gmail.com>
2025-05-25 13:08:27 -0700

    Fix issues with resources config

--- main.tf
+++ main.tf
@@ -73,9 +73,8 @@ resource "aws_instance" "web-server" {
 
   ami                  = "ami-0953476d60561c955"
   instance_type        = "t2.micro"
-  subnet_id            = module.vpc.private_subnets[0]
-  availability_zone    = "us-east-1a"
-  security_groups      = [aws_security_group.ssm_sg.id]
+  subnet_id            = module.vpc.private_subnets[count.index]
+  security_groups      = [aws_security_group.ec2_ssm_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
   user_data            = <<-EOF
               #!/bin/bash
@@ -93,34 +92,67 @@ resource "aws_instance" "web-server" {
 }
 
 #security groups
-resource "aws_security_group" "ssm_sg" {
+#ec2 sg
+resource "aws_security_group" "ec2_ssm_sg" {
   name        = "allow_ssm"
-  description = "Allow connection to SSM"
+  description = "Security group for EC2 instances allowing outbound SSM communication."
   vpc_id      = module.vpc.vpc_id
 
   tags = {
-    Name = "ssm-sg"
+    Name = "EC2-SSM-Instance-SG"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.ssm_sg.id
-  cidr_ipv4         = "10.0.1.0/24"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+resource "aws_vpc_security_group_egress_rule" "allow_endpoints_https_out" {
+  security_group_id            = aws_security_group.ec2_ssm_sg.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.ssm_vpc_endpoint_sg.id
+  description                  = "Allow outbound HTTPS to SSM endpoints"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id            = aws_security_group.ssm_sg.id
-  referenced_security_group_id = aws_security_group.ssm_sg.id
-  ip_protocol                  = "-1"
+  security_group_id = aws_security_group.ec2_ssm_sg.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = module.vpc.vpc_cidr_block
+  description       = "Allows HTTP from within VPC for web server"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.ssm_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_ec2sg_in" {
+  security_group_id            = aws_security_group.ec2_ssm_sg.id
+  referenced_security_group_id = aws_security_group.ec2_ssm_sg.id
+  ip_protocol                  = "-1"
+  description                  = "Allow all traffic from within itself"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_out" {
+  security_group_id = aws_security_group.ec2_ssm_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
+  description       = "Allow all egress traffic"
+}
+
+#vpc sg
+resource "aws_security_group" "ssm_vpc_endpoint_sg" {
+  name        = "ssm-vpc-endpoint-sg"
+  description = "Security group for SSM VPC Endpoints"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "SSM-VPC-Endpoint-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ec2_in" {
+  security_group_id            = aws_security_group.ssm_vpc_endpoint_sg.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.ec2_ssm_sg.id
+  description                  = "Allow HTTPS from EC2 instances for SSM"
 }
 
 #SSM Endpoints
@@ -129,7 +161,7 @@ resource "aws_vpc_endpoint" "ssm_endpoint" {
   subnet_ids          = module.vpc.private_subnets
   service_name        = "com.amazonaws.us-east-1.ssm"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.ssm_sg.id]
+  security_group_ids  = [aws_security_group.ssm_vpc_endpoint_sg.id]
   private_dns_enabled = true
 
   tags = {
@@ -142,7 +174,7 @@ resource "aws_vpc_endpoint" "ssmmessages_endpoint" {
   subnet_ids          = module.vpc.private_subnets
   service_name        = "com.amazonaws.us-east-1.ssmmessages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.ssm_sg.id]
+  security_group_ids  = [aws_security_group.ssm_vpc_endpoint_sg.id]
   private_dns_enabled = true
 
   tags = {
@@ -155,7 +187,7 @@ resource "aws_vpc_endpoint" "ec2messages_endpoint" {
   subnet_ids          = module.vpc.private_subnets
   service_name        = "com.amazonaws.us-east-1.ec2messages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.ssm_sg.id]
+  security_group_ids  = [aws_security_group.ssm_vpc_endpoint_sg.id]
   private_dns_enabled = true
 
   tags = {
